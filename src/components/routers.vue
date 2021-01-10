cac4dc228fa2ba3b9fc480e64ffd958ae5db088a
sahev <s4mus.sh@gmail.com>
2021-01-10 16:36:29 -0300

    first layout

---
+++ src/components/routers.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    routers
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'routers',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
